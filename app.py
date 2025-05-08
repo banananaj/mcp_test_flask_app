c1413dad1169eb18349c313a6e0938c9101960ca
gidong <gidong@lgcns.com>
2025-05-08 16:12:21 +0900

    message change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Flask!'
+    return 'Hello, Flask! commit'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
